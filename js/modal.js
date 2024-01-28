b6cfb5ecd766c3d60c800c0ce0e08c3e16d5fea3
Anano Topuria <anano.topuriaa@gmail.com>
2024-01-28 16:02:27 +0400

    feat: add sliding animation to modal and fix: ensure correct modal close behavior

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,36 @@
 export const openModalWindow = function () {
-  const modal = document.querySelector(`.modal`);
-  const overlayy = document.querySelector(`.overlayy`);
-  const openModal = document.querySelectorAll(`.open-modal`);
+  const modal = document.querySelector('.modal');
+  const overlayy = document.querySelector('.overlayy');
+  const openModal = document.querySelectorAll('.open-modal');
   for (let i = 0; i < openModal.length; i++) {
-    openModal[i].addEventListener(`click`, function () {
-      modal.classList.remove(`hidden`);
-      overlayy.classList.remove(`hidden`);
-    });
+      openModal[i].addEventListener('click', function () {
+          modal.classList.remove('hidden');
+          overlayy.classList.remove('hidden');
+          modal.classList.add('slide-in');
+      });
   }
 };
 
 export const closeModalWindow = function () {
-    document.addEventListener('click', function (event) {
-        const btnClose = event.target.closest('.close-modal');
-        if (btnClose) {
-            const modal = btnClose.closest('.modal');
-            const overlayy = modal.querySelector('.overlayy');
-            modal.classList.add('hidden');
-            overlayy.classList.add('hidden');
-        }
-    });
+  document.addEventListener('click', function (event) {
+      const btnClose = event.target.closest('.close-modal');
+      const modal = document.querySelector('.modal');
+      const overlayy = document.querySelector('.overlayy'); 
+      if (btnClose || event.target === overlayy) {
+          event.preventDefault();
+          modal.classList.remove('slide-in');
+          modal.classList.add('hidden');
+          overlayy.classList.add('hidden');
+      }
+  });
 };
 
 export const overlayModal = function () {
-  const modal = document.querySelector(`.modal`);
-  const overlayy = document.querySelector(`.overlayy`);
-  overlayy.addEventListener(`click`, function () {
-    modal.classList.add(`hidden`);
-    overlayy.classList.add(`hidden`);
+  const modal = document.querySelector('.modal');
+  const overlayy = document.querySelector('.overlayy');
+  overlayy.addEventListener('click', function () {
+      modal.classList.remove('slide-in');
+      modal.classList.add('hidden');
+      overlayy.classList.add('hidden');
   });
 };
